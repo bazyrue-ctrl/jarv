c7d10156eb03be39466d30787ce0d6f9a5e226f9
bazyrue-ctrl <bazyrue@gmail.com>
2026-05-19 13:01:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='static', template_folder='templates')
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
 # Home page
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# Serve images (if needed)
-@app.route('/images/<path:filename>')
-def images(filename):
-    return send_from_directory('static/images', filename)
-
-# Serve JS (if needed)
-@app.route('/js/<path:filename>')
-def js_files(filename):
-    return send_from_directory('static/js', filename)
-
 if __name__ == "__main__":
-    # Host 0.0.0.0 so it works on most cloud deployments
     app.run(host="0.0.0.0", port=5000, debug=True)
